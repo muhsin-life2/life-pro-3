7ba505010143b43f006b20aab35a07877a05647e
muhsin-life2 <128311823+muhsin-life2@users.noreply.github.com>
2023-03-31 10:37:29 +0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['life-cdn.lifepharmacy.com', 'lifeadmin-app.s3.me-south-1.amazonaws.com', 'www.lifepharmacy.com'],
+    domains: ['life-cdn.lifepharmacy.com', 'lifeadmin-app.s3.me-south-1.amazonaws.com', 'www.lifepharmacy.com', 'www.life-me.com'],
   },
   experimental: {
     appDir: true,
